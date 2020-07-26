5c091dbcc381193dd436249c6b4b76835ce07bc2
liu cong <297978715@qq.com>
2020-07-27 04:07:47 +0800

    提交

---
+++ miniprogram/pages/editUserInfo/name/name.js
@@ -0,0 +1,107 @@
+// miniprogram/pages/editUserInfo/name/name.js
+
+const app = getApp()
+const db = wx.cloud.database()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    goodsName : ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    this.setData({
+      goodsName : app.userInfo.goodsName
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  handleText(ev) {
+    let value = ev.detail.value;
+    this.setData({
+      goodsName: value
+    });
+  },
+  handleBtn(ev) {
+    this.updategoodsName();
+  },
+  updategoodsName() {
+    wx.showLoading({
+      title: '更新中'
+    })
+    db.collection('users').doc(app.userInfo._id).update({
+      data: {
+        goodsName: this.data.goodsName
+      }
+    }).then((res) => {
+      wx.hideLoading();
+      wx.showToast({
+        title: '更新成功'
+      });
+      app.userInfo.goodsName = this.data.goodsName;
+    });
+  },
+  bindGetUserInfo(ev) {
+    let userInfo = ev.detail.userInfo;
+    if(userInfo){
+      this.setData({
+        goodsName : userInfo.goodsName
+      },()=>{
+        this.updategoodsName();
+      });
+    }
+  }
+})
